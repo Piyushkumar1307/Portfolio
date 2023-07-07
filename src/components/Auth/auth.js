f06ec438688aefe308c19cdc033cd5bfd44e9cdd
Piyushkumar1307 <tretaeduction20@gmail.com>
2023-07-08 04:10:00 +0530

    new lines

--- src/components/Auth/auth.js
+++ src/components/Auth/auth.js
@@ -30,7 +30,7 @@ const Auth = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/login", {
+      const response = await fetch("https://portfolio-backend-em5j.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- src/components/Register/register.js
+++ src/components/Register/register.js
@@ -19,7 +19,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/register", {
+      const response = await fetch("https://portfolio-backend-em5j.vercel.app//register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
